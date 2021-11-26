603d157b56cbc055b24a36f2e134dfee27926da5
Sidd Bairan <bairansa@gmail.com>
2021-11-26 22:13:57 +0800

    Avoid returning to "All" if project isn't deleted

--- src/scripts/modules/edit-mode.js
+++ src/scripts/modules/edit-mode.js
@@ -1,13 +1,19 @@
 import { projectsHandler } from "./projects";
 
 const projectEditMode = (function () {
+  // Avoids returning to "All" if the project isn't deleted
+  const checkIfSelectedProjectIsDeleted = (project) => {
+    if (project.parentNode.parentNode.classList.contains('active-project')) {
+      projectsHandler.setAllProjectAsDefault();
+    }
+  };
 
   const deleteProject = (project) => {
     const projectContainer = document.querySelector('[data-projects]')
     // Convert the children of the projectContainer into an array using the spread [...] operator then get the index of the selected list
     const index = [...projectContainer.children].indexOf(project.parentNode.parentNode);
     projectsHandler.projectsArray.splice(index, 1);
-    projectsHandler.setAllProjectAsDefault();
+    checkIfSelectedProjectIsDeleted(project);
     projectsHandler.persistToLocalStorage();
   };
 
